45a75a5273d9a516daeefe7bb61c8bb29721b18a
Henzo Gomes <henzo.gomes@gmail.com>
2021-07-03 12:02:51 -0300

    WIP aula 3

--- src/services/CreateUserService.ts
+++ src/services/CreateUserService.ts
@@ -2,38 +2,38 @@ import { getCustomRepository } from "typeorm"
 import { UsersRepositories } from "../repositories/UsersRepositories"
 
 interface IUserRequest {
-    name: string
-    email: string
-    admin?: boolean
+	name: string
+	email: string
+	admin?: boolean
 }
 
 class CreateUserService {
 
-    async execute({ name, email, admin }: IUserRequest) {
-        const usersRepository = getCustomRepository(UsersRepositories)
+	async execute({ name, email, admin }: IUserRequest) {
+		const usersRepository = getCustomRepository(UsersRepositories)
 
-        if (!email) {
-            throw new Error("Email incorrect")
-        }
+		if (!email) {
+			throw new Error("Email incorrect")
+		}
 
-        const userAlreadyExists = await usersRepository.findOne({
-            email
-        })
+		const userAlreadyExists = await usersRepository.findOne({
+			email
+		})
 
-        if (userAlreadyExists) {
-            throw new Error("User already exists")
-        }
+		if (userAlreadyExists) {
+			throw new Error("User already exists")
+		}
 
-        const user = usersRepository.create({
-            name,
-            email,
-            admin
-        })
+		const user = usersRepository.create({
+			name,
+			email,
+			admin
+		})
 
-        await usersRepository.save(user)
+		await usersRepository.save(user)
 
-        return user
-    }
+		return user
+	}
 }
 
 export { CreateUserService }
